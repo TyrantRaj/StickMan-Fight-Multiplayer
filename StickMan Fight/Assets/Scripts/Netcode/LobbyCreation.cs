a28eb59745c34d745fa0e6a1c3c4e06343549688
TyrantRaj <tyrantraj@gmail.com>
2024-09-20 19:18:47 +0530

    added RELAY

--- StickMan Fight/Assets/Scripts/Netcode/LobbyCreation.cs
+++ StickMan Fight/Assets/Scripts/Netcode/LobbyCreation.cs
@@ -9,10 +9,17 @@ using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using System.Threading.Tasks;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 
 
 public class LobbyCreation : MonoBehaviour
 {
+    private const string KEY_RELAY_JOIN_CODE = "RelayJoinCode";
+
     [SerializeField] private SceneChanger changer;
     [SerializeField] private Button createLobbyBtn;
     [SerializeField] private Button quickJoinBtn;
@@ -97,6 +104,52 @@ public class LobbyCreation : MonoBehaviour
 
     }
 
+    private async Task<Allocation> AllocateRelay()
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(Max_Players - 1);
+            return allocation;
+        }
+        catch (RelayServiceException e)
+        {
+            
+            Debug.Log(e);
+            return default;
+            
+        }
+
+        
+    }
+
+    private async Task<string> GetRelayJoinCode(Allocation allocation)
+    {
+        try
+        {
+            string relayJoinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            return relayJoinCode;
+        }
+        catch (RelayServiceException e) { 
+            Debug.Log(e);
+            return default;
+        }
+        
+    }
+
+    private async Task<JoinAllocation> JoinRelay(string joincode)
+    {
+        try
+        {
+            JoinAllocation joinallocation = await RelayService.Instance.JoinAllocationAsync(joincode);
+            return joinallocation;
+        }
+        catch (RelayServiceException e) {
+            Debug.Log(e);
+            return default;
+        }
+        
+    }
+
     public async void CreateLobby(string lobbyname, bool isPrivate)
     {
         try
@@ -105,6 +158,20 @@ public class LobbyCreation : MonoBehaviour
             {
                 IsPrivate = isPrivate,
             });
+
+            Allocation allocation = await AllocateRelay();
+
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+            string relayJoinCode = await GetRelayJoinCode(allocation);
+
+            await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions {
+                Data = new Dictionary<string, DataObject>
+                {
+                    {KEY_RELAY_JOIN_CODE, new DataObject(DataObject.VisibilityOptions.Member,relayJoinCode)}
+
+                }
+            });
+
             showLog();
             Debug.Log("Host Started");
             NetworkManager.Singleton.StartHost();
@@ -121,7 +188,13 @@ public class LobbyCreation : MonoBehaviour
         try
         {
             joinedLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
-            showLog();
+
+            string relayJoinCode =  joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
+
+            JoinAllocation joinallocation = await JoinRelay(relayJoinCode);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinallocation, "dtls"));
+
+            showLog(); 
             NetworkManager.Singleton.StartClient();
 
         }
@@ -136,6 +209,12 @@ public class LobbyCreation : MonoBehaviour
         try
         {
             joinedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(code);
+
+            string relayJoinCode = joinedLobby.Data[KEY_RELAY_JOIN_CODE].Value;
+
+            JoinAllocation joinallocation = await JoinRelay(relayJoinCode);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinallocation, "dtls"));
+
             NetworkManager.Singleton.StartClient();
         }
         catch (LobbyServiceException e)
